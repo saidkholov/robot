b0559c156451841a999c3cba705604d69a4d4afa
Said <skholov2@gmail.com>
2015-09-18 20:10:59 +1200

    surface module created

---
+++ test/surfaceSpec.js
@@ -0,0 +1,28 @@
+//Surface module test
+
+var expect = require("chai").expect;
+var surface = require("../lib/surface");
+
+
+describe("Surface", function(){
+    describe("#width()", function(){
+        it("should return default width as 5", function() {
+            var width = surface.width();
+            expect(width).to.equal(5);    
+        });
+        it("should set/get width of surface", function() {
+            var width = surface.width(10);
+            expect(width).to.equal(10);    
+        });
+    });
+    describe("#height()", function() {
+        it("should get default height of surface as 5", function() {
+            var height = surface.height();
+            expect(height).to.equal(5); 
+        });
+        it("should set/get height of surface", function() {
+            var height = surface.height(5);
+            expect(height).to.equal(5); 
+        });
+    });
+});
