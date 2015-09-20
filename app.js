c7cefd76765edc2c3935cbd5df5167a093dc37cb
Said <skholov2@gmail.com>
2015-09-20 16:04:14 +1200

    --file feature added

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
+#!/usr/bin/env node
+
 // Application's main entry point
 // Usage: ./app.js {args}
 // ==========================================================================
 
-#! /usr/bin/env node
-
 // Load minimist - helps to parse user specified arguments
-var argv = require("minimist")(process.argv.slice(2), {string : "place"});
+var minimist = require("minimist");
+var argv = minimist(process.argv.slice(2), {string : "place"});
 
 var robot = require("./lib/robotFacade");
 var help = require("./lib/help.js");
 
-// on --place argument specified
+// when --file argument specified override further instructions
+if(argv.file) {
+	fs = require('fs');
+	try {
+		var input  = fs.readFileSync(argv.file).toString();
+	} catch(e) {
+		console.error(e);
+		process.exit(0);
+	}
+	var commands = input.split(" ");
+	argv = minimist(commands, {string : "place"});
+}
+
+// when --place argument specified
 if (argv.place) {
 	var args = argv.place.split(',');
 	robot.place(args);
 }
 
-// on --help argument specified 
+// when --help argument specified 
 if (argv.help) {
 	help.showHelp();
 }
 
 // Do Robot actions if robot is placed i.e. --place is called 
 if (robot.onSurface()) {
-	for (var i = 0; i <=argv['_'].length - 1; i++) {
-		switch(argv['_'][i]) {
+	for (var i = 0; i <=argv._.length - 1; i++) {
+		switch(argv._[i]) {
 			case "left" :
 				robot.left();
 				break;
@@ -41,5 +55,4 @@ if (robot.onSurface()) {
 				help.showHelp();
 		}
 	}
-}
-		
+}		
